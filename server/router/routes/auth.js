c59d387ec7e0d6c258e83be46e9da595f78dcc5f
shkfnly <ashoka.finley@gmail.com>
2015-08-19 23:10:42 -0400

    .gitignore should work

--- server/router/routes/auth.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var moment = require('moment');
-var _ = require('underscore');
-var auth = require('./authLogic.js')
-
-router.post('/', auth.login)
-
-module.exports = router;
