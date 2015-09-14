00f568fcc6d65a9e89790b8d3a9f96f9636a956d
shkfnly <ashoka.finley@gmail.com>
2015-09-14 11:10:43 -0700

    fixed files

--- server/sample_data/link_connections.js
+++
@@ -1,7 +0,0 @@
-function links(start1, end1, start2, end2){
-  for(var i = 0; i < 200; i++){
-    node1 = Math.floor(Math.random() * (end1 - start1) + start1);
-    node2 = Math.floor(Math.random() * (end2 - start2) + start2);
-    db.relate(node1, 'flows_to', node2, function(err, relationship){})
-  }
-}
