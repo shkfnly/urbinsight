fc191d38e2404287ae7331ac202e1e6b6ffd2d7b
shkfnly <ashoka.finley@gmail.com>
2016-01-29 00:39:49 -0800

    Vector Tiles now rendering correctly for mapbox-gl

--- server/router/routes/lots.js
+++ server/router/routes/lots.js
@@ -9,25 +9,26 @@ var mapnik = require('mapnik');
 var zlib = require('zlib');
 var VectorTile = require('vector-tile').VectorTile;
 var Protobuf = require('pbf');
+var bboxpolygon = require('turf-bbox-polygon');
 mapnik.register_default_fonts();
 mapnik.register_default_input_plugins();
 
 var mercator = new SphericalMercator({
-  size: 512
+  size: 256
 });
 
-router.get('/:city_name/:z/:x/:y.pbf', function(req, res) {
-
-  var city_name = req.params.city_name;
-  var bbox = mercator.bbox(
-      +req.params.x,
-      +req.params.y,
-      +req.params.z,
-      false,
-      '4326'
-      );
-
-  pg.connect(conString, function(err, client, done){
+var toGeoJSON = function(json) {
+  var geojson = {type: "FeatureCollection", features: []}
+  json.forEach(function(feature) {
+    var obj = {
+      "type": "Feature",
+      "properties": JSON.parse(JSON.stringify(feature.properties)),
+      "geometry": JSON.parse(feature.geojson),
+    }
+    geojson.features.push(obj)
+  })
+  return geojson;
+};
     var config_object = {
       'medellin' : {
         'attributes': ['cobama', 'subtipo_lote', 'tipo_lote', 'estrato', 'zona', 'usopredial', 'npisos', 'calificacion']
@@ -39,10 +40,27 @@ router.get('/:city_name/:z/:x/:y.pbf', function(req, res) {
         'attributes': ['id', 'tags']
       }
     };
+
+router.get('/:city_name/:z/:x/:y.mvt', function(req, res) {
+  var city_name = req.params.city_name;
+  var bbox = mercator.bbox(
+      +req.params.x,
+      +req.params.y,
+      +req.params.z,
+      false,
+      '4326'
+      );
+
+    var poly = JSON.stringify(bboxpolygon(bbox).geometry);
+    var bounds = 'ST_SetSRID(ST_GeomFromGeoJSON(\'' + poly + '\'), 4326)'
+    var sql = 'select st_asgeojson(wkb_geometry) as geojson, row_to_json((select l from (select ' + config_object[city_name]['attributes'].join(', ') + ' ) as l )) as properties from ' + city_name + "_parcels";
+    sql += ' where st_intersects(wkb_geometry, ' + bounds + ')';
+    //client.query("SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.wkb_geometry)::json As geometry, row_to_json((SELECT l FROM (SELECT " + config_object[city_name]['attributes'].join(', ') + " ) As l )) As properties FROM " + city_name + "_parcels As lg WHERE st_intersects(lg.wkb_geometry, st_makeenvelope(" + bbox.toString() + ", 4326) ) ) As f )  As fc", function(err, result){
+
+  pg.connect(conString, function(err, client, done){
     var handleError = function(err) {
       // no error occurred, continue with the request
       if(!err) return false;
-
       // An error occurred, remove the client from the connection pool.
       // A truthy value passed to done will remove the connection from the pool
       // instead of simply returning it to be reused.
@@ -57,38 +75,35 @@ router.get('/:city_name/:z/:x/:y.pbf', function(req, res) {
       // res.end('An error occurred');
       return true;
     };
-
     // handle an error from the connection
-    if(handleError(err)) return;
+    //if(handleError(err)) return console.error('error running query', err);
     if(city_name === 'medellin' || city_name === 'lima' || city_name === 'budapest'){
-    client.query("SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.wkb_geometry)::json As geometry, row_to_json((SELECT l FROM (SELECT " + config_object[city_name]['attributes'].join(', ') + " ) As l )) As properties FROM " + city_name + "_parcels As lg WHERE st_intersects(lg.wkb_geometry, st_makeenvelope(" + bbox.toString() + ", 4326) ) ) As f )  As fc", function(err, result){
-      if (err) {
-        console.log("error in the query");
-        return console.error('error running query', err);
-      }
-      // handle an error from the connection
-      if(handleError(err)) return;
-
-      var vtile = new mapnik.VectorTile(+req.params.z, +req.params.x, +req.params.y);
-      if( (typeof result.rows[0] !== 'undefined')  && (result.rows[0].row_to_json.features !== null )){
-          try {
-
-            vtile.addGeoJSON(JSON.stringify(result.rows[0].row_to_json), 'lots');
-          } catch (e) {
-            console.log("came back with an error");
-            console.log(e);
-          }
-      }
-      res.setHeader('Content-Encoding', 'deflate');
-      res.setHeader('Content-Type', 'application/x-protobuf');
-      zlib.deflate(vtile.getData(), function(err, pbf) {
-        done();
-        // res.writeHead(200);
-        res.send(pbf);
-      });    
-    });
+      client.query(sql, [], function(err, result) {
+        done()
+        if(err) {
+          res.end();
+          return console.error('error running query', err);
+        }
+        if (result.rows.length) {
+          var data = result.rows
+        } else {
+          var data = {};
+        }
+        var vtile = new mapnik.VectorTile(+req.params.z, +req.params.x, +req.params.y);
+        try {
+          // The second name is the name of the sub layer that must be accessed in GL
+          vtile.addGeoJSON(JSON.stringify(toGeoJSON(result.rows)), 'parcels');
+        } catch (e) {
+          console.log(e);
+        }
+        res.setHeader('Content-Encoding', 'gzip');
+        res.setHeader('Content-Type', 'application/x-protobuf');
+        zlib.gzip(vtile.getData(), function(err, pbf) {
+          res.send(pbf);
+        });    
+      });
     }
-  });
+  })
 });
 
 router.get('/lot/:city_name/', function(req, res){
@@ -103,7 +118,14 @@ router.get('/lot/:city_name/', function(req, res){
 });
 
 module.exports = router;
+      // handle an error from the connection
+      /*if( (typeof result.rows[0] !== 'undefined')  && (result.rows[0].row_to_json.features !== null )){
+          try {
+
+          } catch (e) {
+            console.log("came back with an error");
+            console.log(e);
+          }
+      }*/
 // "select st_asgeojson(wkb_geometry) as feature from budapest2 where st_intersects(wkb_geometry, st_makeenvelope(" +
 // bbox.toString() + ", 4326)) LIMIT 1", function(err, result){
-// select st_asgeojson(wkb_geometry) as feature from budapest where ST_CONTAINS(ST_GeometryFromText('POLYGON((18.96137237548828 47.402299753699616, 19.186248779296875 47.402299753699616, 19.186248779296875 47.58069447747099, 18.96137237548828 47.58069447747099, 18.96137237548828 47.402299753699616))', 4326), wkb_geometry);
-// select st_asgeojson(wkb_geometry) as feature from budapest where ST_Contains(ST_GeometryFromText('POLYGON((18.964462280273438 47.40880590168841, 19.264869689941406 47.40880590168841, 19.264869689941406 47.58717856130284, 18.964462280273438 47.58717856130284, 18.964462280273438 47.40880590168841))', 4326), wkb_geometry)
